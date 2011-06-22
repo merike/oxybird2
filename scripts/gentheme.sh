105e952ac59c5089a2440ae467ab43618cb7b136
Vimalkumar Velayudhan <vimalkumarvelayudhan@gmail.com>
2011-06-22 19:08:59 +0100

    Added: changes from merike/oxybird2 Fixed: incorrect file name in gentheme.sh

--- scripts/gentheme.sh
+++ scripts/gentheme.sh
@@ -3,4 +3,4 @@ cd ../oxybird-linux-2.1/
 rm oxybird-linux-2.1.jar
 rm chrome/OxyBird2.jar
 zip -r chrome/OxyBird2.jar aero calendar communicator editor global lightning messenger messenger-newsblog mozapps
-zip -r oxybird-win-2.1.jar chrome chrome.manifest icon.png install.rdf preview.png
+zip -r oxybird-linux-2.1.jar chrome chrome.manifest icon.png install.rdf preview.png
