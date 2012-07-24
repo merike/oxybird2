2b10f114441d7726f064fbf355ee89e1c3111065
Merike Sell <merikes@gmail.com>
2012-07-24 17:29:56 +0300

    Changes for 15 VI

--- scripts/gentheme.sh
+++ scripts/gentheme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ../oxybird-linux-2.1/
-rm oxybird-linux-2.1.jar
+rm oxybird-linux.jar
 mkdir chrome
 zip -r chrome/OxyBird2.jar aero calendar communicator editor global lightning messenger messenger-newsblog mozapps
-zip -r oxybird-linux-2.1.jar chrome chrome.manifest icon.png install.rdf preview.png
+zip -r oxybird-linux.jar chrome chrome.manifest icon.png install.rdf preview.png
 rm -r chrome
