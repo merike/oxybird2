6b2b3f0eafbf06386367ababafd4766a2422d632
Merike Sell <merikes@gmail.com>
2013-01-13 15:25:23 +0200

    /global & /mozapps

--- scripts/gentheme.sh
+++ scripts/gentheme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ../oxybird-linux/
-rm oxybird-linux.jar
+rm oxybird.jar
+cd oxybird-linux
 mkdir chrome
 zip -r chrome/OxyBird2.jar calendar communicator editor global lightning messenger messenger-newsblog mozapps
-zip -r oxybird-linux.jar chrome chrome.manifest icon.png install.rdf preview.png
+zip -r ../oxybird.jar chrome chrome.manifest icon.png install.rdf preview.png
 rm -r chrome
